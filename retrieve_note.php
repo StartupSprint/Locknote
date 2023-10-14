f9b051fbc366325d6a0aa67d49a5c596d591b1ab
devkiraa <devkiraa@gmail.com.>
2023-10-14 13:30:17 +0530

    Files Updated

--- retrieve_note.php
+++ retrieve_note.php
@@ -1,10 +1,12 @@
 <?php
 $key = $_POST['key'];
+
 // Database connection settings
 $servername = "sql211.infinityfree.com";
 $username = "if0_35085005";
 $password = "z93UAb75vWW";
 $dbname = "if0_35085005_locknote";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
@@ -13,6 +15,7 @@ if ($conn->connect_error) {
 
 // Escape the input to prevent SQL injection (better to use prepared statements)
 $key = $conn->real_escape_string($key);
+
 // Query the database to retrieve the note based on the key
 $sql = "SELECT note FROM notes WHERE code = '$key'";
 $result = $conn->query($sql);
@@ -23,5 +26,6 @@ if ($result->num_rows > 0) {
 } else {
     echo 'not_found';
 }
+
 $conn->close();
 ?>
